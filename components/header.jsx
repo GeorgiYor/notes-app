943c50beddee4962f1b1dd21ac6729966292d02a
GeorgiYor <georgi.yordanov359@gmail.com>
2024-11-04 22:46:42 +0200

    feat(initial design): added first components and some basic scetches/designs for them

---
+++ components/header.jsx
@@ -0,0 +1,12 @@
+import '../src/resources/header.scss';
+
+function Header() {
+    return (
+     <div className="header">
+        
+     </div>
+    )
+}
+  
+export default Header
+  
---
+++ components/sidebar.jsx
@@ -0,0 +1,18 @@
+import '../src/resources/sidebar.scss';
+
+function Sidebar() {
+    return (
+     <div className="sidebar">
+        <div className="notes">
+            <div className="notes__categories">
+                <div className="categories__accordion">
+                    
+                </div>
+            </div>
+        </div>
+     </div>
+    )
+}
+  
+export default Sidebar
+  
